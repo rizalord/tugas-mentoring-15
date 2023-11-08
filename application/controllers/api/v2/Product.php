c0fd5ab839aed5f51009fb14eb0bdc77e51a4efe
rizalord <ahmadkhamdani9@gmail.com>
2023-11-08 20:05:19 +0700

    Remove unnecessary headers from Product controller constructor

--- application/controllers/api/v2/Product.php
+++ application/controllers/api/v2/Product.php
@@ -19,8 +19,6 @@ class Product extends REST_Controller
      */
     public function __construct()
     {
-        header('Access-Control-Allow-Origin: *');
-        header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
         parent::__construct();
         $this->load->library('Authorization_Token');
         $this->load->model('Product_model');
